84c1e4d106fdb80a9ec5d8a4cd1ae4f78db9d11f
WavyCpp <aldeennoor2007@gmail.com>
2026-08-24 18:19:04 +0300

    rearrange

---
+++ T003/client/tcp_client.py
@@ -0,0 +1,67 @@
+from socket import *
+
+SERVER_NAME = "localhost"
+SERVER_PORT = 5780
+
+while True:
+
+    choice = input("""
+========================
+Network Diagnostic System
+========================
+1. Ping Host
+2. Trace Route
+3. DNS Lookup
+4. IP Configuration
+5. Routing Table
+6. ARP Table
+7. Active TCP Connections
+8. Exit
+Select: """)
+
+    if choice == "1":
+        command = "ping"
+
+    elif choice == "2":
+        command = "tracert"
+
+    elif choice == "3":
+        command = "nslookup"
+
+    elif choice == "4":
+        command = "ipconfig"
+
+    elif choice == "5":
+        command = "route"
+
+    elif choice == "6":
+        command = "arp"
+
+    elif choice == "7":
+        command = "netstat"
+
+    elif choice == "8":
+        command = "exit"
+
+    else:
+        print("Invalid choice")
+        continue
+
+    if command in ["ping", "tracert", "nslookup"]:
+        host = input("Enter host: ")
+        command = command + " " + host
+
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((SERVER_NAME, SERVER_PORT))
+
+    client_socket.send(command.encode())
+
+    response = client_socket.recv(4096).decode()
+
+    print("Server response:")
+    print(response)
+
+    client_socket.close()
+
+    if choice == "8":
+        break
