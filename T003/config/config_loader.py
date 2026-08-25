687bd3ceeb34cf69f5308a5e7939ba112b0146f5
Mohammad Hasiba <mhasiba56@gmail.com>
2026-08-25 21:32:41 +0300

    semi changes

--- T003/config/config_loader.py
+++ T003/config/config_loader.py
@@ -1,5 +1,5 @@
 import json
 
 def load_config():
-    with open("T003/config.json", "r") as f:     
+    with open("config.json", "r") as f:
           return json.load(f)
--- T003/server/logger.py
+++ T003/server/logger.py
@@ -1,7 +1,10 @@
 import json
+from pathlib import Path
 
 
 def log_request(data, log_file):
+
+    log_file = Path(__file__).parent.parent / log_file
 
     with open(log_file, "r") as f:
         logs = json.load(f)
