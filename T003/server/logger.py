a7b6aea290a678c36f6804e9d3ef17dd4be7e8ef
Mohammad Hasiba <mhasiba56@gmail.com>
2026-08-25 10:35:29 +0300

    changes

--- T003/server/logger.py
+++ T003/server/logger.py
@@ -1,5 +1,11 @@
 import json
+import os
 
-def log_request(entry, log_file="logs/server_log.json"):
-    with open(log_file, "a") as f:  # append
-        f.write(json.dumps(entry) + "\n") # entry: info about the request // dumps: convent from dect -> JSON string
+def log_request(data):
+    os.makedirs("logs", exist_ok=True)
+
+    log_file = "logs/server_log.json"
+
+    with open(log_file, "a") as f:
+        json.dump(data, f)
+        f.write("\n")
